f825a67951f405fc7c632fba22399ab55feca08f
Michael Rapson <rpsmic001@gmail.com>
2024-07-14 00:38:36 +0200

    Integrate login button

---
+++ za.co.sagoclubs.CaptureResults/src/za/co/sagoclubs/Cognito.java
@@ -0,0 +1,74 @@
+package za.co.sagoclubs;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
+import com.amazonaws.regions.Regions;
+
+public class Cognito {
+	private String poolID = "us-east-2_lQ07vxzvD";
+	private String clientID = "6pqaqv2diseq4dq1pt3o72mcn6";
+	private String clientSecret = null;
+	private Regions awsRegion = Regions.US_EAST_2;
+	private CognitoUserPool userPool;
+	private Context appContext;
+	private String userPassword;
+
+	public Cognito(Context context){
+		appContext = context;
+		userPool = new CognitoUserPool(context, this.poolID, this.clientID, this.clientSecret, this.awsRegion);
+	}
+
+	public void userLogin(String userId, String password){
+		CognitoUser cognitoUser =  userPool.getUser(userId);
+		this.userPassword = password;
+		cognitoUser.getSessionInBackground(authenticationHandler);
+	}
+
+	// Callback handler for the sign-in process
+	AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
+		@Override
+		public void authenticationChallenge(ChallengeContinuation continuation) {
+
+		}
+
+		@Override
+		public void onSuccess(CognitoUserSession userSession, CognitoDevice newDevice) {
+			Toast.makeText(appContext, "Sign in success", Toast.LENGTH_LONG).show();
+
+		}
+
+		@Override
+		public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String userId) {
+			// The API needs user sign-in credentials to continue
+			AuthenticationDetails authenticationDetails = new AuthenticationDetails(userId, userPassword, null);
+			// Pass the user sign-in credentials to the continuation
+			authenticationContinuation.setAuthenticationDetails(authenticationDetails);
+			// Allow the sign-in to continue
+			authenticationContinuation.continueTask();
+		}
+
+		@Override
+		public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
+			// Multi-factor authentication is required; get the verification code from user
+			//multiFactorAuthenticationContinuation.setMfaCode(mfaVerificationCode);
+			// Allow the sign-in process to continue
+			//multiFactorAuthenticationContinuation.continueTask();
+		}
+
+		@Override
+		public void onFailure(Exception exception) {
+			// Sign-in failed, check exception for the cause
+			Toast.makeText(appContext,"Sign in Failure", Toast.LENGTH_LONG).show();
+		}
+	};
+}
--- za.co.sagoclubs.CaptureResults/src/za/co/sagoclubs/LoginActivity.java
+++ za.co.sagoclubs.CaptureResults/src/za/co/sagoclubs/LoginActivity.java
@@ -2,6 +2,7 @@ package za.co.sagoclubs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,5 +22,15 @@ public class LoginActivity extends Activity {
 		etPassword = findViewById(R.id.etPassword);
 		btnLogin = findViewById(R.id.btnLogin);
 		txtForgetPass= findViewById(R.id.txtForgotPass);
+
+		btnLogin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Cognito authentication = new Cognito(getApplicationContext());
+				authentication.userLogin(
+						etUsername.getText().toString().trim(),
+						etPassword.getText().toString());
+			}
+		});
 	}
 }
