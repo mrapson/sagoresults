3671e54389a4da0414a651df9bc18bbf8de6f4fc
Michael Rapson <rpsmic001@gmail.com>
2024-07-17 23:14:58 +0200

    More clean up

--- za.co.sagoclubs.CaptureResults/src/za/co/sagoclubs/Cognito.java
+++ za.co.sagoclubs.CaptureResults/src/za/co/sagoclubs/Cognito.java
@@ -15,61 +15,61 @@ import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.Authentic
 import com.amazonaws.regions.Regions;
 
 public class Cognito {
-	private final CognitoUserPool userPool;
-	private final Context appContext;
+    private final CognitoUserPool userPool;
+    private final Context appContext;
 
-	private final UserData userData;
+    private final UserData userData;
 
-	public Cognito(Context context){
-		appContext = context;
-		String poolID = "us-east-2_lQ07vxzvD";
-		String clientID = "6pqaqv2diseq4dq1pt3o72mcn6";
-		Regions awsRegion = Regions.US_EAST_2;
-		String clientSecret = null;
+    public Cognito(Context context) {
+        appContext = context;
+        String poolID = "us-east-2_lQ07vxzvD";
+        String clientID = "6pqaqv2diseq4dq1pt3o72mcn6";
+        Regions awsRegion = Regions.US_EAST_2;
+        String clientSecret = null;
 
-		userPool = new CognitoUserPool(context, poolID, clientID, clientSecret, awsRegion);
-		userData = UserData.getInstance();
-	}
+        userPool = new CognitoUserPool(context, poolID, clientID, clientSecret, awsRegion);
+        userData = UserData.getInstance();
+    }
 
-	public void userLogin(){
-		CognitoUser cognitoUser =  userPool.getUser(userData.getUsername());
-		cognitoUser.getSessionInBackground(authenticationHandler);
-	}
+    public void userLogin() {
+        CognitoUser cognitoUser = userPool.getUser(userData.getUsername());
+        cognitoUser.getSessionInBackground(authenticationHandler);
+    }
 
-	// Callback handler for the sign-in process
-	AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
-		@Override
-		public void authenticationChallenge(ChallengeContinuation continuation) {
+    // Callback handler for the sign-in process
+    AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
+        @Override
+        public void authenticationChallenge(ChallengeContinuation continuation) {
 
-		}
+        }
 
-		@Override
-		public void onSuccess(CognitoUserSession userSession, CognitoDevice newDevice) {
-			// Sign-in was successful, cognitoUserSession will contain tokens for the user
-			userData.setIdToken(userSession.getIdToken());
-			Toast.makeText(appContext, "Sign in success", Toast.LENGTH_LONG).show();
-		}
+        @Override
+        public void onSuccess(CognitoUserSession userSession, CognitoDevice newDevice) {
+            // Sign-in was successful, cognitoUserSession will contain tokens for the user
+            userData.setIdToken(userSession.getIdToken());
+            Toast.makeText(appContext, "Sign in success", Toast.LENGTH_LONG).show();
+        }
 
-		@Override
-		public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String userId) {
-			// The API needs user sign-in credentials to continue
-			AuthenticationDetails authenticationDetails = new AuthenticationDetails(userId, userData.getPassword(), null);
-			// Pass the user sign-in credentials to the continuation
-			authenticationContinuation.setAuthenticationDetails(authenticationDetails);
-			// Allow the sign-in to continue
-			authenticationContinuation.continueTask();
-		}
+        @Override
+        public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String userId) {
+            // The API needs user sign-in credentials to continue
+            AuthenticationDetails authenticationDetails = new AuthenticationDetails(userId, userData.getPassword(), null);
+            // Pass the user sign-in credentials to the continuation
+            authenticationContinuation.setAuthenticationDetails(authenticationDetails);
+            // Allow the sign-in to continue
+            authenticationContinuation.continueTask();
+        }
 
-		@Override
-		public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
-			// Not used. See the link below for an implementation example.
-			// https://aws.amazon.com/blogs/mobile/using-android-sdk-with-amazon-cognito-your-user-pools/
-		}
+        @Override
+        public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
+            // Not used. See the link below for an implementation example.
+            // https://aws.amazon.com/blogs/mobile/using-android-sdk-with-amazon-cognito-your-user-pools/
+        }
 
-		@Override
-		public void onFailure(Exception exception) {
-			// Sign-in failed, check exception for the cause
-			Toast.makeText(appContext,"Sign in Failure", Toast.LENGTH_LONG).show();
-		}
-	};
+        @Override
+        public void onFailure(Exception exception) {
+            // Sign-in failed, check exception for the cause
+            Toast.makeText(appContext, "Sign in Failure", Toast.LENGTH_LONG).show();
+        }
+    };
 }
