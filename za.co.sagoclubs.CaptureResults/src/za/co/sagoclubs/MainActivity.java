30a0ce8dbe4f3b9bc954b5bfcaca84cd5f8fda14
Michael Rapson <rpsmic001@gmail.com>
2024-08-15 23:39:20 +0200

    Disable player actions until players loaded

--- za.co.sagoclubs.CaptureResults/src/za/co/sagoclubs/MainActivity.java
+++ za.co.sagoclubs.CaptureResults/src/za/co/sagoclubs/MainActivity.java
@@ -1,13 +1,19 @@
 package za.co.sagoclubs;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+
+public class MainActivity extends AppCompatActivity {
     private final UserData userData = UserData.getInstance();
+
+    private Button btnCaptureResult;
+    private Button btnDisplayLogFile;
 
     @Override
     public void onResume() {
@@ -29,28 +35,63 @@ public class MainActivity extends Activity {
         btnPlayerRatings.setEnabled(true);
         btnPlayerRatings.setOnClickListener(v -> {
             Intent myIntent = new Intent(v.getContext(), PlayerRatingsActivity.class);
-            startActivityForResult(myIntent, 0);
+            startActivity(myIntent);
         });
 
-        Button btnCaptureResult = findViewById(R.id.btnCaptureResult);
-        btnCaptureResult.setVisibility(userData.isGuestUser() ? View.INVISIBLE : View.VISIBLE);
-        btnCaptureResult.setOnClickListener(v -> {
-            Result.setResultState(ResultState.Enter);
-            Intent myIntent = new Intent(v.getContext(), SelectWhitePlayerActivity.class);
-            startActivityForResult(myIntent, 0);
-        });
+        btnCaptureResult = findViewById(R.id.btnCaptureResult);
+        btnDisplayLogFile = findViewById(R.id.btnDisplayLogFile);
 
-        Button btnDisplayLogFile = findViewById(R.id.btnDisplayLogFile);
-        btnDisplayLogFile.setVisibility(userData.isGuestUser() ? View.INVISIBLE : View.VISIBLE);
-        btnDisplayLogFile.setOnClickListener(v -> {
-            Intent myIntent = new Intent(v.getContext(), SelectLogPlayerActivity.class);
-            startActivityForResult(myIntent, 0);
-        });
+        PlayerUseCase.getInstance()
+                .getPlayerData()
+                .observe(this, (playerData -> {
+                    boolean showButtons = !userData.isGuestUser();
+                    boolean clickable = playerData != null && playerData.hasData();
+                    setupButton(
+                            btnCaptureResult,
+                            AppCompatResources.getDrawable(this, R.drawable.green_button),
+                            v -> {
+                                Result.setResultState(ResultState.Enter);
+                                Intent myIntent = new Intent(v.getContext(),
+                                        SelectWhitePlayerActivity.class);
+                                startActivity(myIntent);
+                            },
+                            showButtons,
+                            clickable
+                    );
+                    setupButton(
+                            btnDisplayLogFile,
+                            AppCompatResources.getDrawable(this, R.drawable.purple_button),
+                            v -> {
+                                Intent myIntent = new Intent(v.getContext(),
+                                        SelectLogPlayerActivity.class);
+                                startActivity(myIntent);
+                            },
+                            showButtons,
+                            clickable
+                    );
+                }));
 
         Button btnSettings = findViewById(R.id.btnSettings);
         btnSettings.setOnClickListener(v -> {
             Intent myIntent = new Intent(v.getContext(), SettingsActivity.class);
-            startActivityForResult(myIntent, 0);
+            startActivity(myIntent);
         });
+    }
+
+    private void setupButton(Button button, Drawable activeBackground,
+                             View.OnClickListener listener,
+                             boolean visible, boolean clickable) {
+        button.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+
+        if (clickable) {
+            button.setBackground(activeBackground);
+            button.setOnClickListener(listener);
+        } else {
+            Drawable greyButton = AppCompatResources.getDrawable(
+                    this,
+                    R.drawable.grey_button);
+            button.setBackground(greyButton);
+            button.setClickable(false);
+        }
     }
 }
